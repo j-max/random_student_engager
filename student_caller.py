36d4bd1715a0b8415db0bcf9178806180d75d3d0
Max <jmax.barry@gmail.com>
2021-07-25 17:04:12 -0600

    Add replace=False so that students aren't chosen multiple times

--- student_caller.py
+++ student_caller.py
@@ -1,18 +1,5 @@
 import numpy as np
 
-def n_random_students(student_list, number_of_students=1,  question = None):
-    """
-    :param student_list: a list of students in any given class
-    :return: a student to be called on
-    """
-
-    students =  np.random.choice(student_list, number_of_students)
-
-    if question is not None:
-        print(question)
-
-    return students
-
 class cohort_caller:
 
     def __init__(self, student_list):
@@ -23,13 +10,20 @@ class cohort_caller:
 
     def n_random_students(self, number_of_students=1,  question = None):
 
+        """TODO describe function
+
+        :param number_of_students: 
+        :param question: 
+        :returns: 
+
+        """
         if len(self.mutable_student_list) < number_of_students:
             print(self.mutable_student_list)
 
             self.mutable_student_list = self.student_list.copy()
 
 
-        choice =  np.random.choice(self.mutable_student_list, number_of_students)
+        choice =  np.random.choice(self.mutable_student_list, number_of_students, replace=False)
         self.mutable_student_list = [student for student in self.mutable_student_list
                                      if student not in choice]
 
