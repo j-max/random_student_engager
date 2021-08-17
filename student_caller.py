24508b7987960dfbbe6b879f05b698b307241754
Max <jmax.barry@gmail.com>
2021-08-17 17:42:38 -0500

    Add function to reduce repetition

--- student_caller.py
+++ student_caller.py
@@ -59,44 +59,40 @@ class CohortCaller:
          If there is an odd number of students, the odd student will
          be added to the last group'''
 
-        # Choose a random pair from the available pairs
-        
-        # Remove all pairs with either student from the remaining pairs
-
-
-
         # if the class has an odd number of students
         if len(self.student_list)%2:
             number_of_pairs = len(self.student_list)//2
+            # Choose a random pair from the available pairs
 
-            activity_pairs = np.random.choice(self.mutable_pairs, number_of_pairs,
-                                            replace=False)
+            activity_pairs = []
+            pair_choices_remaining = self.mutable_pairs.copy()
 
-            selected_students = set([student for pair in activity_pairs
-                                for student in pair.split(' ')])
+            while len(activity_pairs) < number_of_pairs:
+                pair_choice = np.random.choice(pair_choices_remaining)
+                activity_pairs.append(pair_choice)
 
-            # if not all students were selected, recreate the pairs
-            # subtract 1 from len because of odd number of students 
-            while len(selected_students) != len(self.student_list)-1:
-                activity_pairs = np.random.choice(self.mutable_pairs, number_of_pairs,
-                                            replace=False)
-                selected_students = set([student for pair in activity_pairs
-                                for student in pair.split(' ')])
+                # make a copy of mutable pairs. This is a temporary list of pairs to choose from.
+                pairs_to_remove = pair_in_list(pair_choice, pair_choices_remaining)
+                pair_choices_remaining = [pair for pair in pair_choices_remaining
+                                          if pair not in pairs_to_remove]
 
 
-            # Add statement if selected list != len of students, then regenerate pairs
-            # for classes with an odd number of students
-            for pair, group in zip(activity_pairs, range(1,len(activity_pairs)+1)):
+            print(activity_pairs)
+
+            for pair,group in zip(activity_pairs, list(range(1,number_of_pairs+1))):
                 print('Group ' + str(group) + ': Driver --> ' + pair + ' <--Navigator')
 
-                self.mutable_pairs.remove(pair)
+            selected_students = []
 
-            # with odd number of students, one student won't be selected
-            # print out and add to the last group
+            for pair in activity_pairs:
+                selected_students.extend(pair.split(' '))
+
             student_not_selected = [student for student in self.student_list
                                     if student not in selected_students]
 
             print('Group ' + str(group) + ': ' + student_not_selected[0])
+
+
 
 
         # if the class has an even number of students
@@ -123,6 +119,37 @@ class CohortCaller:
                     print('Group ' + str(group) + ': Driver --> ' + pair + ' <--Navigator')
                     self.mutable_pairs.remove(pair)
 
+def pair_in_list(chosen_pair, possible_pairs):
 
+    ''' Given a chosen tuple of students
+        find all remaining pairs that include
+        either of the students
+
+    params:
+    chosen_pair: a string of students in a pair
+    possible_pairs: a list of strings representing student pairs
+
+    '''
+    pairs_to_remove = []
+    student_1, student_2 = chosen_pair.split(' ')
+    for pair in possible_pairs:
+        pair_1, pair_2 = pair.split(' ')
+
+        if pair_1 == student_1:
+            pairs_to_remove.append(pair)
+        elif pair_1 == student_2:
+            pairs_to_remove.append(pair)
+        elif pair_2 == student_1:
+            pairs_to_remove.append(pair)
+        elif pair_2 == student_2:
+            pairs_to_remove.append(pair)
+        else:
+            continue
+
+    return pairs_to_remove
+
+# Test pair_in_list
 test_cohort = CohortCaller(avocoder_toasters)
 test_cohort.generate_pairs()
+remove_pair_test = pair_in_list('Raylin Ronak', test_cohort.mutable_pairs)
+
