ef744a116ae49cabfda7c163677e039e32181dd9
Max <jmax.barry@gmail.com>
2021-08-17 18:11:18 -0500

    lint student_caller

--- student_caller.py
+++ student_caller.py
@@ -1,6 +1,6 @@
 import numpy as np
 from itertools import permutations
-from student_list import avocoder_toasters
+
 
 class CohortCaller:
 
@@ -15,11 +15,13 @@ class CohortCaller:
 
         """
         Instruct n_random_students to select a given number of students
-        from the class list. 
+        from the class list.
 
-        :param number_of_students: The number of students to choose in each call
+        :params:
+            number_of_students:The number of students to choose in each call
         :returns:
 
+          choice: an array students to be called on
         """
 
         # Reset the list if there are not enough students to be called
@@ -28,8 +30,8 @@ class CohortCaller:
 
             self.mutable_student_list = self.student_list.copy()
 
-        choice =  np.random.choice(self.mutable_student_list,
-                                   number_of_students, replace=False)
+        choice = np.random.choice(self.mutable_student_list,
+                                  number_of_students, replace=False)
 
         self.mutable_student_list = [student for student in
                                      self.mutable_student_list if
@@ -38,7 +40,6 @@ class CohortCaller:
         return choice
 
     def generate_pairs(self):
-
 
         '''
         Pairs of students are generated from the student list using itertools
@@ -50,10 +51,11 @@ class CohortCaller:
         That way every pair activity is ensured to have a different pairing.
         '''
 
-        if self.mutable_pairs == None:
+        if self.mutable_pairs is None:
             # np.random.choice doesn't work when choosing tuples from a list
             # so pair tupples are converted to strings
-            self.all_pairs = [pair[0] +' '+ pair[1] for pair in self.all_pairs]
+            self.all_pairs = [pair[0] + ' ' + pair[1]
+                              for pair in self.all_pairs]
             self.mutable_pairs = self.all_pairs.copy()
 
         '''The number of pairs should be the total number of students
@@ -72,28 +74,32 @@ class CohortCaller:
             pair_choice = np.random.choice(pair_choices_remaining)
             activity_pairs.append(pair_choice)
 
-            # make a copy of mutable pairs. This is a temporary list of pairs to choose from.
+            # Make a copy of mutable pairs.
+            # This is a temporary list of pairs to choose from.
             pairs_to_remove = pair_in_list(pair_choice, pair_choices_remaining)
             pair_choices_remaining = [pair for pair in pair_choices_remaining
-                                        if pair not in pairs_to_remove]
+                                      if pair not in pairs_to_remove]
 
-        for pair,group in zip(activity_pairs, list(range(1,number_of_pairs+1))):
+        for pair, group in zip(activity_pairs,
+                               list(range(1, number_of_pairs+1))
+                               ):
             print('Group ' + str(group) + ': Driver --> ' + pair + ' <--Navigator')
 
-        if len(self.student_list)%2:
+        if len(self.student_list) % 2:
 
             selected_students = []
 
             for pair in activity_pairs:
                 selected_students.extend(pair.split(' '))
 
-
             student_not_selected = [student for student in self.student_list
                                     if student not in selected_students]
 
             print('Group ' + str(group) + ': ' + student_not_selected[0])
 
-        self.mutable_pairs = [pair for pair in self.mutable_pairs if pair not in activity_pairs]
+        self.mutable_pairs = [pair for pair in self.mutable_pairs
+                              if pair not in activity_pairs]
+
 
 def pair_in_list(chosen_pair, possible_pairs):
 
@@ -101,9 +107,9 @@ def pair_in_list(chosen_pair, possible_pairs):
         find all remaining pairs that include
         either of the students
 
-    params:
-    chosen_pair: a string of students in a pair
-    possible_pairs: a list of strings representing student pairs
+        params:
+        chosen_pair: a string of students in a pair
+        possible_pairs: a list of strings representing student pairs
 
     '''
     pairs_to_remove = []
@@ -123,13 +129,3 @@ def pair_in_list(chosen_pair, possible_pairs):
             continue
 
     return pairs_to_remove
-
-# Test pair_in_list
-#odd_test_cohort = CohortCaller(avocoder_toasters)
-#odd_test_cohort.generate_pairs()
-#remove_pair_test = pair_in_list('Raylin Ronak', test_cohort.mutable_pairs)
-#
-# avocoder_toasters.pop()
-# even_test_cohort = CohortCaller(avocoder_toasters)
-# even_test_cohort.generate_pairs()
-
